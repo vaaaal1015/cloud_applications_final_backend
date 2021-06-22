a9ae5bffa39a1bdb4d9d07945e2d3a1eef2d3026
107590022 <t107590022@ntut.org.tw>
2021-06-23 05:08:39 +0800

    finish

--- src/main/java/com/example/demo/controller/VotController.java
+++ src/main/java/com/example/demo/controller/VotController.java
@@ -4,10 +4,13 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import com.example.demo.entity.Paper;
 import com.example.demo.entity.Vot;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,26 +27,55 @@ public class VotController {
     private List<Vot> DB = new ArrayList<>();
     private List<Paper> paperDB = new ArrayList<>();
 
+    @CrossOrigin
     @PostConstruct
     private void initDB() {
-        List<String> list = new ArrayList<>();
-        list.add("option 1");
-        list.add("option 2");
-        list.add("option 3");
+        List<String> list;
 
-        DB.add(new Vot(1, "title", list));
-        DB.add(new Vot(2, "title", list));
-        DB.add(new Vot(3, "title", list));
-        DB.add(new Vot(4, "title", list));
-        DB.add(new Vot(5, "title", list));
+        list = new ArrayList<>();
+        list.add("必定有錢剩");
+        list.add("鐵柱磨成針");
+        list.add("天下無敵人");
+        list.add("點石可成金");
+        DB.add(new Vot(1, "只要有恆心 的下一句是什麼？", list));
 
-        paperDB.add(new Paper(1, 3));
-        paperDB.add(new Paper(1, 3));
-        paperDB.add(new Paper(1, 2));
-        paperDB.add(new Paper(1, 1));
-        paperDB.add(new Paper(1, 3));
+        list = new ArrayList<>();
+        list.add("宋朝");
+        list.add("元朝");
+        list.add("唐朝");
+        list.add("清朝");
+        DB.add(new Vot(2, "垂簾聽政始於何時？", list));
+
+        list = new ArrayList<>();
+        list.add("是");
+        list.add("不是");
+        DB.add(new Vot(3, "郭老是不是好老師", list));
+
+        list = new ArrayList<>();
+        list.add("是");
+        list.add("當然是");
+        DB.add(new Vot(4, "這是最棒的投票網站", list));
+
+        list = new ArrayList<>();
+        list.add("科科科科科科科科科科科");
+        list.add("呵呵呵呵呵呵呵呵呵呵呵");
+        list.add(":)");
+        DB.add(new Vot(5, "哈哈哈哈哈哈哈哈哈哈哈哈哈哈", list));
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 4);
+        map.put(2, 4);
+        map.put(3, 2);
+        map.put(4, 2);
+        map.put(5, 2);
+        for (int i = 0; i < 500; i++) {
+            int id = (int) (Math.random() * 5) + 1;
+            int op = (int) (Math.random() * map.get(id)) + 1;
+            paperDB.add(new Paper(id, op));
+        }
     }
 
+    @CrossOrigin
     @GetMapping("/vots/{id}")
     public ResponseEntity<Vot> getVot(@PathVariable("id") int id) {
         for (Vot vot : DB) {
@@ -56,6 +88,7 @@ public class VotController {
 
     }
 
+    @CrossOrigin
     @GetMapping("/result/{id}")
     public ResponseEntity<List<Integer>> getVotResult(@PathVariable("id") int id) {
         List<Integer> count = new ArrayList<>();
@@ -76,6 +109,7 @@ public class VotController {
         return ResponseEntity.notFound().build();
     }
 
+    @CrossOrigin
     @GetMapping("/vots")
     public ResponseEntity<List<Vot>> getVots() {
 
@@ -89,16 +123,26 @@ public class VotController {
         return ResponseEntity.ok().body(DB);
     }
 
+    @CrossOrigin
     @PostMapping("/create")
-    public ResponseEntity<Vot> createVot(@RequestBody Vot request) {
+    public ResponseEntity<Integer> createVot(@RequestBody Vot request) {
 
         Vot vot = new Vot(DB.size() + 1, request.getTitle(), request.getOption());
         DB.add(vot);
 
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(vot.getId()).toUri();
+        // URI location = ServletUriComponentsBuilder.fromCurrentServletMapping().path("/vots/{id}")
+        //         .buildAndExpand(vot.getId()).toUri();
 
-        return ResponseEntity.created(location).body(vot);
+        return ResponseEntity.ok().body(vot.getId());
+    }
+
+    @CrossOrigin
+    @PostMapping("/voting")
+    public ResponseEntity<String> voting(@RequestBody Paper request) {
+        Paper paper = new Paper(request.getId(), request.getSelect());
+        paperDB.add(paper);
+
+        return ResponseEntity.ok().body("OK");
     }
 
 }
